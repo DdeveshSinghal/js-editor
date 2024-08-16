b600efe0271c8e6530cfb4782d0b14d6dea018b7
Devesh Singhal <121570826+DdeveshSinghal@users.noreply.github.com>
2024-08-16 17:39:22 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+// alert(3 +3)  // we are using node js, not browser
+
+/*
+    number => 2 to power 53
+    bigInt
+    String => " "
+    boolean => true/false
+    null => standalon value
+    undefined
+    symbol => unique
+*/
+
+let name = "devesh"
+let age = 20
+let accessAccount = false
+
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
