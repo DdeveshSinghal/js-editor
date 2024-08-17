72c00b5e4093a26e369b27980f4b50198a8b5d16
Devesh Singhal <121570826+DdeveshSinghal@users.noreply.github.com>
2024-08-17 11:01:34 +0000

    datatype summary

---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,25 @@
+//  Primitive DataType
+
+/*    
+    7 Types:    String -> string
+                Number -> number
+                Boolean -> boolean
+                Null    -> object
+                Undefined -> undefined
+                Symbol  -> symbol
+                BigInt -> bigint
+*/
+
+const id = Symbol('123')
+console.log(typeof id);
+
+// Reference (Non-Primitive)
+
+/*
+    Array -> object
+    Objects -> object
+    Function -> function
+*/
+
+
+// Javascript - dynamic typed langugage
