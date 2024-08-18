cb482e8ede16dfc04f1918f00c4e703476c22acb
Devesh Singhal <121570826+DdeveshSinghal@users.noreply.github.com>
2024-08-18 14:39:01 +0000

    number and maths

---
+++ 01_basics/06_number_and_maths.js
@@ -0,0 +1,34 @@
+const score = 600
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));    
+
+// const otherNumber = 123.866
+// console.log(otherNumber.toPrecision(4));    // round-off
+
+// const commas = 1000000
+// console.log(commas.toLocaleString('en-IN'));    // separate the numbers with commas
+
+// console.log(Number.MAX_VALUE);
+
+
+// ******************* Maths ****************************
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.2));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+
+console.log(Math.sqrt(5));
+
+console.log(Math.floor(Math.random() * 10) + 1);
+
+// formula for random value to identifie
+const min = 10
+const max = 20
+
+console.log(Math.floor((Math.random() * (max - min + 1)) + min))
