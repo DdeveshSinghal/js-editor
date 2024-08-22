1ea28bfdac67dcefdb6b4205398dc77103eabac2
Devesh Singhal <121570826+DdeveshSinghal@users.noreply.github.com>
2024-08-22 13:08:53 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -1,11 +1,16 @@
+var c = 300     // global scope
 
-
-{
+let a = 200
+{                           // block scope
     let a = 10
     const b = 20
     var c = 30
+
+    console.log("Inner: ", a);
+    
 }
 
 // console.log(a);
 // console.log(b);
-console.log(c);
+// console.log(c);
+console.log("outside: ", a);
