385cda9922b8a069d758470b47bfc84f14d505fd
Devesh Singhal <121570826+DdeveshSinghal@users.noreply.github.com>
2024-08-22 15:21:52 +0000

    scope level

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,11 +6,50 @@ let a = 200
     const b = 20
     var c = 30
 
-    console.log("Inner: ", a);
+    // console.log("Inner: ", a);
     
 }
 
 // console.log(a);
 // console.log(b);
 // console.log(c);
-console.log("outside: ", a);
+// console.log("outside: ", a);
+
+
+
+
+function one(){
+    const username = "devesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+
+if (true) {
+    const username = "devesh"
+    if (username === "devesh") {
+        const website = " instagram"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+function addOne(num){
+    return num+1
+}
+addOne(5)
+
+// addTwo(5)        /* function do not call before writing a expression function only after a function*/
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
