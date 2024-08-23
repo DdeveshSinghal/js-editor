b13ca41e33cce1ac908bcde9f8cb103e7f2dd22c
Devesh Singhal <121570826+DdeveshSinghal@users.noreply.github.com>
2024-08-23 16:44:24 +0000

    loops

---
+++ 05_iterations/01_loop.js
@@ -0,0 +1,14 @@
+// for
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        
+        console.log(i,j)
+    }
+}
